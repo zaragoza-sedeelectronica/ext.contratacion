4161f3526864adcaa3fa4a5c1393c7c7d5dffb22
Víctor <vmorlan@zaragoza.es>
2020-05-04 12:30:38 +0200

    sincronizacion repositorio

--- opencity.ext.contratacion/src/main/java/org/sede/servicio/perfilcontratante/ContratoController.java
+++ opencity.ext.contratacion/src/main/java/org/sede/servicio/perfilcontratante/ContratoController.java
@@ -37,7 +37,7 @@ import java.util.*;
 
 @Gcz(servicio="PERFILCONTRATANTE", seccion="CONTRATO")
 @Controller
-//@Description("Ayuntamiento: Contratación pública")
+@Description("Ayuntamiento: Contratación pública")
 @Transactional(Esquema.TMPERFILCONTRATANTE)
 @RequestMapping(value = "/" + ContratoController.MAPPING, method = RequestMethod.GET)
 public class ContratoController {
